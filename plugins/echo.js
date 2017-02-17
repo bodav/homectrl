08efbc3de3c13b1cfad25d9684e52bae315ec5e3
bodav <bodavidsen@gmail.com>
2017-02-17 11:00:02 +0100

    Added simple test echo plugin

---
+++ plugins/echo.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let winston = require("winston");
+
+module.exports.initialize = (bus) => {
+    winston.info("Initializing Echo plugin...");
+
+    bus.on("echo.ping", (payload) => {
+        winston.debug("[Event][echo.ping]: " + payload);
+        bus.emit("echo.pong", payload);
+    });
+
+    winston.info("Echo plugin initialized");
+};
+
+module.exports.destroy = () => {
+    winston.info("Echo plugin destroyed");
+};
+
+module.exports.info = () => {
+    return {
+        name: "Echo",
+        capabilities: [
+            { event: "echo.ping", direction: "inbound", payload: "any" },
+            { event: "echo.pong", direction: "outbound", payload: "any" }
+        ]
+    }
+};
