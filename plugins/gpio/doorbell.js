4069a57bb797a35e72c141b8df22a8dc44766179
bodav <bodavidsen@gmail.com>
2017-02-17 10:59:02 +0100

    Added code tested on devpi

--- plugins/gpio/doorbell.js
+++ plugins/gpio/doorbell.js
@@ -1,1 +1,52 @@
 "use strict";
+
+let winston = require("winston");
+let gpio = require('rpi-gpio');
+
+const INPUTPIN = 7;
+
+let changeThrottle = false;
+
+module.exports.initialize = (bus) => {
+    winston.info("Initializing Doorbell plugin...");
+
+    gpio.setup(INPUTPIN, gpio.DIR_IN, gpio.EDGE_RISING, (err) => {
+        if(err != undefined) {
+            winston.error("Error setting up gpio pin: " + INPUTPIN);
+            winston.error(err);
+        }
+
+        gpio.on("change", (channel, val) => {
+            if(!changeThrottle) {
+                changeThrottle = true;
+                winston.debug("Doorbell plugin - Got rising edge event");
+                bus.emit("Doorbell", {
+                    pin: channel,
+                    value: val
+                 });
+
+                setTimeout(() => {
+                    winston.debug("Doorbell plugin - Resetting event throttle flag");
+                    changeThrottle = false;
+                }, 4500);
+            }
+        });
+    });
+
+    winston.info("Doorbell plugin initialized");
+};
+
+module.exports.destroy = () => {
+    winston.info("Destroying Doorbell plugin");
+    gpio.destroy();
+    winston.info("Doorbell plugin destroyed!");
+};
+
+module.exports.info = () => {
+    return {
+        name: "GPIO-Doorbell",
+        capabilities: [
+            { event: "Doorbell", direction: "outbound", payload: "None" }
+        ]
+    }
+};
